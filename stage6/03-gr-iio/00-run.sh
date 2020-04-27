analogdevicesinc/adi-kuiper-gen

commit 879b8c03e386acfc8b4ea572b0c58015e9d6ce2b

stage6: Add gr-iio to the build
This patch adds the gnuradio iio blocks that can be used with ADI products.

Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

stage6/03-gr-iio/00-run.sh
@@ -1,0 +1,14 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+
+git clone https://github.com/analogdevicesinc/gr-iio.git
+pushd gr-iio
+cmake .
+make
+sudo make install
+popd
+ldconfig
+
+rm -rf gr-iio
+EOF

commit d1e311b94c57ca48d54fe9d508a236e62b31b64b

stage6: gr-iio: Fix install prefix for gr-iio
This patch will set the install prefix for cmake configuration to /usr.

Fixes: 879b8c03e386 ("stage6: Add gr-iio to the build")
Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

stage6/03-gr-iio/00-run.sh
@@ -4,7 +4,7 @@ on_chroot << EOF
 
 git clone https://github.com/analogdevicesinc/gr-iio.git
 pushd gr-iio
-cmake .
+cmake -DCMAKE_INSTALL_PREFIX=/usr .
 make -j $NUM_JOBS
 sudo make install
 popd