8fe836c95b4c75e505b3cea9344cd3d3dd3eafec
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2022-03-31 13:31:28 +0300

    stage7: Build all the kernels locally
    The additional features enabled by 'stage7/files/kuiper_defconfig'
    should be included in all kernels used by Kuiper.
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

--- stage7/01-adi-kernel-dts/00-run.sh
+++ stage7/01-adi-kernel-dts/00-run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
+
 SCRIPTS_DIR=wiki-scripts
 LINUX="linux-adi"
 
+declare -a klist=("zynq" "zynqmp" "socfpga")
 declare -a rpi_configs=("rpi" "2709" "2711")
 declare -a rpi_knames=("" "7" "7l")
 
 build_rpi_linux() {
 
-	# this will checkout the branch that has newest commit - should use a parameter/variable to be able to switch between branches
 	git checkout $(git branch -a --sort=-committerdate | grep -E 'rpi\-[0-9]{1,2}\.[0-9]{1,2}\.y$' | head -1 | sed -e 's/^remotes\/origin\///' -e 's/* //')
 
 	unset KCFLAGS ARCH CROSS_COMPILE
@@ -20,8 +21,23 @@ build_rpi_linux() {
 
 	cp -f arch/$ARCH/boot/dts/overlays/*.dtb* $STAGE_WORK_DIR/rootfs/boot/overlays
 	cp -f arch/$ARCH/boot/dts/bcm27*.dtb $STAGE_WORK_DIR/rootfs/boot
-
 }
+
+build_linux() {
+
+	[ -z $LINUX_BRANCH ] && {
+		LINUX_BRANCH=master
+	}
+
+	git checkout $LINUX_BRANCH
+
+	unset KCFLAGS ARCH CROSS_COMPILE DEFCONFIG
+	make clean
+	source $WORK_DIR/$SCRIPTS_DIR/linux/build_$1_kernel_image.sh "$WORK_DIR/$LINUX" "" ""
+	mkdir -p $WORK_DIR/boot_partition/$1-common
+	mv arch/$ARCH/boot/*Image $WORK_DIR/boot_partition/$1-common
+}
+
 pushd "$WORK_DIR"
 
 [ -d "$SCRIPTS_DIR" ] || {
@@ -39,7 +55,12 @@ for i in ${!rpi_configs[@]}; do
 	build_rpi_linux adi_bcm${rpi_configs[$i]}_defconfig kernel${rpi_knames[$i]}.img
 done
 
-popd 1> /dev/null # push ${LINUX}
-popd 1> /dev/null # push ${WORK_DIR}
+for kernel in ${klist[@]}; do
+	echo "building for $kernel"
+	build_linux $kernel
+done
 
-echo "RPI Kernel build finished."
+popd 1> /dev/null
+popd 1> /dev/null
+
+echo "Kernel build finished."
