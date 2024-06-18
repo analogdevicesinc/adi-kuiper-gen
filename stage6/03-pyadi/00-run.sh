810fab8d21e4728d48b1fca041cbf75cc63c76c3
Stefan Raus <Stefan.Raus@analog.com>
2024-06-18 10:02:54 +0100

    stage6/03-pyadi:install pyadi-iio from latest main
    Temporary switch the installation of pyadi-iio from last release tag to
    latest main, because the most recent release is to old.
    Latest main contains support for iio-osc navasa profile generator and
    other fixes, required in next release.
    
    Signed-off-by: Stefan Raus <Stefan.Raus@analog.com>

--- stage6/03-pyadi/00-run.sh
+++ stage6/03-pyadi/00-run.sh
@@ -2,7 +2,10 @@
 
 on_chroot << EOF
 
-pip3 install pyadi-iio
+# Temporary switch installation of pyadi-iio from last release to latest main
+# pip3 install pyadi-iio
+pip3 install git+https://github.com/analogdevicesinc/pyadi-iio.git
+
 pip3 install git+https://github.com/analogdevicesinc/pyadi-dt.git
 echo "export LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}:/usr/local/lib\"" >> /home/analog/.bashrc
 ldconfig
