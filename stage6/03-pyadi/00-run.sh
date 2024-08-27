f820e1b25b5bf6d107c280541257bad779a90db7
Stefan Raus <Stefan.Raus@analog.com>
2024-08-27 11:48:51 +0100

    stage6/03-pyadi: switch installation to latest tag
    Since there was create a new release tag for pyadi-iio,
    switch the installation to it, instead of latest main.
    
    Signed-off-by: Stefan Raus <Stefan.Raus@analog.com>
    (cherry picked from commit db2b10cdaad7257ebc3a4d6610481b862ccf3fce)

--- stage6/03-pyadi/00-run.sh
+++ stage6/03-pyadi/00-run.sh
@@ -2,7 +2,11 @@
 
 on_chroot << EOF
 
+# Install latest release tag of pyadi-iio
 pip3 install pyadi-iio
+# If you want to switch the installation from last release tag to latest main, use next line
+#pip3 install git+https://github.com/analogdevicesinc/pyadi-iio.git
+
 pip3 install git+https://github.com/analogdevicesinc/pyadi-dt.git
 echo "export LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}:/usr/local/lib\"" >> /home/analog/.bashrc
 ldconfig
