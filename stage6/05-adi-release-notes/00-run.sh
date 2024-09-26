79341f14e01acc4b78cd43af99bcee803b40a111
mlradu <larisa.radu@analog.com>
2024-09-26 17:00:23 +0300

    stage6/05-adi-release-notes: update release notes to the current Debian version
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stage6/05-adi-release-notes/00-run.sh
+++ stage6/05-adi-release-notes/00-run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
 install -m 755 files/os-release	"${ROOTFS_DIR}/usr/lib/"
+
+OS_RELEASE=$(cat "${ROOTFS_DIR}/etc/debian_version")
+sed -i 's/os_release/$OS_RELEASE/g' "${ROOTFS_DIR}/usr/lib/os-release"
