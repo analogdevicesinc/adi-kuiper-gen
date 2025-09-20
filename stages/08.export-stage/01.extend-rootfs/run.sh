ba19a8bdfd5b91cb0cfb818670b6cc5a0585a2a7
Ioan Dragomir <ioan.dragomir@analog.com>
2025-09-20 19:26:28 +0300

    Reduce boot partition size to 128MB
    DON'T MAINLINE
    
    AD-R1M specific optimization - no need for a 2GB boot partition, we won't
    be storing any carrier board specific boot files, just a few dtbos and a
    ~30MB kernel.
    
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- stages/08.export-stage/01.extend-rootfs/run.sh
+++ stages/08.export-stage/01.extend-rootfs/run.sh
@@ -6,6 +6,9 @@
 # Copyright (c) 2024 Analog Devices, Inc.
 # Author: Larisa Radu <larisa.radu@analog.com>
 
+# 2025-09-19 ioan: bodge to speed up builds on rpi, see corresponding step in 01.bootstrap/run.sh  
+echo > "${BUILD_DIR}"/etc/dpkg/dpkg.cfg.d/unsafe-io
+
 install -m 755 "${BASH_SOURCE%%/run.sh}"/files/extend-rootfs-once "${BUILD_DIR}/etc/init.d/"
 
 # Enable extend-rootfs-oncer service to run at first boot
