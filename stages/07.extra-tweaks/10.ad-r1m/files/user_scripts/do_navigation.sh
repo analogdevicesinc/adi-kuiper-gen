8eb676e4b285362fc0f08292713070737f4cb9ee
Ioan Dragomir <ioan.dragomir@analog.com>
2025-09-25 18:05:38 +0300

    ad-r1m: Use docker compose for managing multiple nodes
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/do_navigation.sh
+++ stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/do_navigation.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-MAP_NAME=map
-I=working
-
-(
-# Make Ctrl-C work
-trap 'kill 0' SIGINT
-
-# Run AMCL localization
-docker run --rm -v ./ros_data:/ros_data --network=host --ipc=host --pid=host $I \
-        ros2 launch adrd_demo_ros2 localization_launch.py map:=/ros_data/maps/$MAP_NAME.yaml &
-
-# Run nav2 stack
-docker run --rm -v ./ros_data:/ros_data --network=host --ipc=host --pid=host $I \
-	ros2 launch adrd_demo_ros2 navigation_launch.py params_file:=/ros_data/navigation_params.yaml &
-)
-
+docker compose up --profile navigation
 
--- stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/start_rviz.sh
+++ stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/start_rviz.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-I=docker.cloudsmith.io/adi/adrd-common/ad-r1m:backpack_rpi5-4380b77
-docker pull $I
-export DISPLAY=:0
 xhost +
-docker run --rm -it --network host --ipc host --pid host -e DISPLAY --privileged --name rviz -v /home/analog/ros_data:/ros_data $I bash -c 'source install/setup.sh; rviz2 -d /ros_data/main.rviz'
+docker compose up --profile rviz
 
