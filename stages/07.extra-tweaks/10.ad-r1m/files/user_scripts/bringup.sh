640c6d258555e534c18d854a9c4eca87dabd3279
Ioan Dragomir <ioan.dragomir@analog.com>
2025-09-25 18:05:38 +0300

    ad-r1m: Use docker compose for managing multiple nodes
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/bringup.sh
+++ stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/bringup.sh
@@ -4,16 +4,18 @@ pushd $(dirname $0)
 
 docker compose down
 
-# Power on CRSF transceiver
+# Power on CRSF transceiver, if not already up
 gpioset 0 21=1
 
-# (Re)start slcan
-./can_setup.sh
- 
+# Reset all CANopen devices - sure way to halt drives and such
+cansend can0 000#8100 # Reset all CANopen devices
+sleep 0.1
+
+# CAN reset
+./recan.sh
+
 # Set IMU frequency
 iio_attr -u ip:localhost -d adis16470 sampling_frequency 100
-
-# Restart IIOD
 systemctl restart iiod.service
 
 docker compose up
--- stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/do_mapping.sh
+++ stages/07.extra-tweaks/10.ad-r1m/files/user_scripts/do_mapping.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-I=working
-docker run --rm -v ./ros_data:/ros_data --network=host --ipc=host --pid=host $I \
-	ros2 launch adrd_demo_ros2 online_async_launch.py params_file:=/ros_data/mapping_params.yaml
+docker compose up --profile mapping
 
