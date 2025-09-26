f1725f822fb4acffd85ba631567897bd0b3ee96d
Ioan Dragomir <ioan.dragomir@analog.com>
2025-09-26 18:23:26 +0300

    ad-r1m: Networking tweaks
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- stages/03.system-tweaks/04.network-tweaks/run.sh
+++ stages/03.system-tweaks/04.network-tweaks/run.sh
@@ -8,8 +8,8 @@
 # Author: Larisa Radu <larisa.radu@analog.com>
 
 # Add custom NetworkManager connection profiles
-install -m 600 "${BASH_SOURCE%%/run.sh}/files/eth0-linklocal"		"${BUILD_DIR}/etc/NetworkManager/system-connections/"
-install -m 600 "${BASH_SOURCE%%/run.sh}/files/Wired connection 1"	"${BUILD_DIR}/etc/NetworkManager/system-connections/"
+install -m 600 "${BASH_SOURCE%%/run.sh}/files/eth0-linklocal"	"${BUILD_DIR}/etc/NetworkManager/system-connections/"
+install -m 600 "${BASH_SOURCE%%/run.sh}/files/eth0-dhcp"	"${BUILD_DIR}/etc/NetworkManager/system-connections/"
 
 chroot "${BUILD_DIR}" << EOF
 	# Link files that enable Predictable Network Interface names to null
