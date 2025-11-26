02951e612c01a3ecb4ef08f1674f826a002514b0
Alisa-Dariana Roman <alisa.roman@analog.com>
2025-11-26 11:58:00 +0200

    University Workshops: add no-os setup

--- stages/07.extra-tweaks/01.extra-scripts/extra-workshops.sh
+++ stages/07.extra-tweaks/01.extra-scripts/extra-workshops.sh
@@ -3,7 +3,7 @@
 #
 # kuiper2.0 - Embedded Linux for Analog Devices Products
 #
-# Copyright (c) 2024 Analog Devices, Inc.
+# Copyright (c) 2025 Analog Devices, Inc.
 # Author: Larisa Radu <larisa.radu@analog.com>
 
 # Install Debian packages
@@ -41,3 +41,59 @@ chmod a+w examples/workshop/pid_control.py
 # Set hostname
 sed -i 's/analog/workshops/g' /etc/hostname
 sed -i 's/analog/workshops/g' /etc/hosts
+
+# Setup no-OS workshop
+cd /home/analog
+git clone https://github.com/romandariana/workshop_baremetal.git
+
+cd workshop_baremetal
+
+WORKSHOP_DIR="/home/analog/workshop_baremetal"
+NO_OS_REPO="https://github.com/romandariana/no-OS.git"
+NO_OS_BRANCH="workshop"
+AI8X_REPO="https://github.com/analogdevicesinc/ai8x-synthesis.git"
+MAX_SDK_ZIP="MAX78000SDK.zip"
+
+cp /stages/07.extra-tweaks/01.extra-scripts/workshops/99-daplink.rules /etc/udev/rules.d/
+
+usermod -a -G tty analog
+
+apt update
+apt install -y \
+    gcc-arm-none-eabi \
+    libusb-0.1-4 \
+    libusb-1.0-0 \
+    libusb-1.0-0-dev \
+    libftdi-dev \
+    libftdi1-dev \
+    libgpiod2 \
+    libhidapi-dev \
+    libhidapi-libusb0 \
+    python3-pygame
+
+mkdir -p "$WORKSHOP_DIR"
+
+if [ -d "$WORKSHOP_DIR/no-OS" ]; then
+    echo "no-OS already exists, skipping"
+else
+    cd "$WORKSHOP_DIR"
+    git clone --depth 2 -b "$NO_OS_BRANCH" "$NO_OS_REPO"
+fi
+
+if [ -d "$WORKSHOP_DIR/ai8x-synthesis" ]; then
+    echo "ai8x-synthesis already exists, skipping"
+else
+    cd "$WORKSHOP_DIR"
+    git clone --depth 1 "$AI8X_REPO"
+fi
+
+if [ -d "$WORKSHOP_DIR/MAX78000SDK" ]; then
+    echo "MAX78000SDK already exists, skipping"
+else
+    cd "$WORKSHOP_DIR"
+    wget https://swdownloads.analog.com/cse/kuiper/kuiperv2.0.0/university-workshops/"$MAX_SDK_ZIP"
+    unzip "$MAX_SDK_ZIP"
+    rm "$MAX_SDK_ZIP"
+fi
+
+chown -R analog:analog /home/analog/workshop_baremetal
